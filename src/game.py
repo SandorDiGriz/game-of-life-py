9fbd6c10c746b72f905dd5f79e7eb25f6fcd743c
Alexander Potekhin <al_potechin@mail.ru>
2022-01-11 04:05:09 +0300

    pep8 changes

--- src/game.py
+++ src/game.py
@@ -1,25 +1,48 @@
+"""File contols the course of the game"""
+
 from grid import Grid
 
+
 class Game:
-    def __init__(self, rows: int, columns: int, mode="auto", birth_chance=25, generations_limit=250):
+    def __init__(
+        self,
+        rows: int,
+        columns: int,
+        mode="auto",
+        birth_chance=25,
+        generations_limit=250,
+    ):
+        """Initialisation of all game parameters
+
+        Args:
+            rows (int): Number of grid's rows
+            columns (int): Number of grid's columns
+            mode (str, optional): Mode of cell generation change ('auto' | 'one-step'). Defaults to "auto".
+            birth_chance (int, optional): [Percentage probability of a cell being born. Defaults to 25.
+            generations_limit (int, optional): Maximum number of generations. Defaults to 250.
+        """
+
         self.rows = rows
         self.columns = columns
         self.mode = mode
         self.birthchace = birth_chance
         self.generations_limit = generations_limit
-        
+
     def run(self):
+        """Controls the two modes of the game"""
         grid = Grid(self.rows, self.columns)
         grid.build_grid()
         game_over = False
+        # Executing first mode
         if self.mode == "auto":
             while not game_over:
                 grid.print_grid()
-                for i in range(self.generations_limit):
+                for _ in range(self.generations_limit):
                     print()
                     grid.evolve()
                     grid.print_grid()
                 game_over = True
+        # Executing second mode
         else:
             while not game_over:
                 grid.print_grid()
@@ -29,4 +52,3 @@ class Game:
                     grid.print_grid()
                     continue
                 game_over = True
-
